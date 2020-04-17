b524b70b1104da706995cb59ab601ff9a19fd7c7
mitchkoj <info@mitchkoj.com>
2020-04-16 20:57:01 -0700

    functioning beta template

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project
 
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    action_on_top = True
+    actions_on_bottom = False
+
+    fields = ('title', 'description', 'technology', 'image')
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+def project_index(request):
+    """
+        An index view that shows a snippet of information
+        about each project.
+    """
+
+    # get all project objects in the database
+    projects = Project.objects.all()
+
+    # dictionary argument for render engine
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    """
+        An detail view that shows more information
+        on a particular topic.
+
+        takes a project id.
+    """
+
+    # get the project with primary key 
+    project = Project.objects.get(pk=pk)
+
+    # dictionary argument for render engine
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
